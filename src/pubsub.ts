f1a897e2bee1fea635e5f8d695cf33b9e3d1671a
Archie G <acgelonga@gmail.com>
2022-09-23 23:36:56 -0500

    Create pubsub

---
+++ src/pubsub.ts
@@ -0,0 +1,24 @@
+export default class PubSub {
+  subscribers: { [key: string]: Function[] };
+
+  constructor() {
+    this.subscribers = {};
+  }
+
+  // Subscribe to an event
+  subscribe(event, callback) {
+    if (this.subscribers[event]) {
+      this.subscribers[event] = [];
+    }
+    this.subscribers[event].push(callback);
+  }
+
+  // Publish an event
+  publish(event) {
+    if (this.subscribers[event]) {
+      this.subscribers[event].forEach((callback) => {
+        callback();
+      });
+    }
+  }
+}
